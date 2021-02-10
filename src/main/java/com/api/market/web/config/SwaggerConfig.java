a8f9eb1a7c6810eec3f37df54928e97f05d8a1de
Ubuntu <ubuntu@ip-172-31-48-205.ec2.internal>
2021-02-10 23:01:38 +0000

    Swagger was implemented in Purchase Controller and the Swagger home page was customized.

--- src/main/java/com/api/market/web/config/SwaggerConfig.java
+++ src/main/java/com/api/market/web/config/SwaggerConfig.java
@@ -2,10 +2,15 @@ package com.api.market.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.Contact;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -16,7 +21,18 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 			.select()
 			.apis(RequestHandlerSelectors.basePackage("com.api.market.web.controller"))
-			.build();	
+			.build()
+			.apiInfo(apiDetails());	
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Market API",
+				"This is the API for manage your market.",
+				"API",
+				"Terms of service",
+				new Contact("Julian Castañeda", "www.julian-castaneda.com", "julian.castaneda@gmail.com"),
+				"License of API", "API license URL", Collections.emptyList());
 	}
 
 }
--- src/main/java/com/api/market/web/controller/PurchaseController.java
+++ src/main/java/com/api/market/web/controller/PurchaseController.java
@@ -1,6 +1,11 @@
 package com.api.market.web.controller;
 
 import com.api.market.domain.Purchase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,19 +15,29 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v0")
+@Api(value = "Purchases", description = "REST API for manage Purchases", tags = { "Purchases" })
 public class PurchaseController {
 
         @GetMapping("/purchases")
+	@ApiOperation("Get all purchases")
+	@ApiResponse(code = 200, message = "OK")
         public ResponseEntity<List<Purchase>> getAllPurchases() {
                 return null;
         }
 
-        @GetMapping("/purchases/clients/{client-id}")
+        @GetMapping("/purchases/customers/{customer-id}")
+	@ApiOperation("Get all customer's purchases")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "OK"),
+		@ApiResponse(code = 404, message = "Purchases not found"),
+	})
         public ResponseEntity<List<Purchase>> getPurchasesByClient(@PathVariable("client-id") String clientId) {
                 return null;
         }
 
         @PostMapping("/purchases")
+	@ApiOperation("Create purchase")
+	@ApiResponse(code = 201, message = "CREATED")
         public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
                 return null;
         }
