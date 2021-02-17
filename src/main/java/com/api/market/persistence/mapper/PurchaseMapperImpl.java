a2a86a41bf7460e29fcf030f20bb04b6c3f9f97b
Julian Castañeda <jccastanedaal@gmail.com>
2021-02-17 22:24:06 +0000

    Se ajusta mapeo de la clase PurchaseMapperImpl

--- src/main/java/com/api/market/persistence/mapper/PurchaseMapperImpl.java
+++ src/main/java/com/api/market/persistence/mapper/PurchaseMapperImpl.java
@@ -71,7 +71,7 @@ public class PurchaseMapperImpl implements PurchaseMapper {
 					}
 					purchase.setItems(items);
 				}
-
+				purchases.add(purchase);
                         }
 		}
 		return purchases;
